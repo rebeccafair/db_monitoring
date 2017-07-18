d697419090ef569687ed55d6df681bc66a431350
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2017-07-18 10:52:34 +0100

    Add Elasticsearch X-Pack installation

--- elk/install_elasticsearch.sh
+++ elk/install_elasticsearch.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Install JDK 8+
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
 "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
@@ -17,4 +19,13 @@ sudo yum install -y elasticsearch
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable elasticsearch.service
 sudo systemctl start elasticsearch.service
+
+# Test elasticsearch
+sudo systemctl status elasticsearch
+
+# Install x-pack for elasticsearch
+ES_HOME=/usr/share/elasticsearch
+(cd $ES_HOME && exec sudo bin/elasticsearch-plugin install --batch x-pack)
+sudo systemctl restart elasticsearch
+sudo systemctl status elasticsearch
 
