f2d86ffc1414d5640352e3749dbb1437c12b4119
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2017-08-01 13:23:47 +0100

    Add Munin client and plugin install

---
+++ munin/client/install_munin_client.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Install EPEL Repository
+wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
+sudo rpm -ivh epel-release-7-9.noarch.rpm
+sudo yum -y update
+
+# Install apache server
+sudo yum install -y htttpd
+sudo systemctl enable httpd
+sudo systemctl start httpd
+
+# Install Munin
+sudo yum install -y munin-node
+sudo cp munin-node.conf /etc/munin/
+sudo munin-node-configure --shell --families=contrib,auto | sh -x
+sudo systemctl enable munin-node
+sudo systemctl start munin-node
+
+# Note: also need to add the new Munin node to the config on the master /etc/munin/munin.conf
+
+# Add Munin user to MySQL
+MYSQL_PASSWORD="Password!1"
+mysql -u root -p$MYSQL_PASSWORD -e "grant process,super on *.* to 'munin'@'localhost' identified by 'Password!1';"
+mysql -u root -p$MYSQL_PASSWORD -e "grant select on mysql.* to 'munin'@'localhost';"
+
+# Install MySQL plugin for Munin
+wget https://github.com/kjellm/munin-mysql/archive/master.zip
+unzip master.zip
+rm -f master.zip
+
+# Install dependencies
+sudo yum group install -y "Development Tools"
+sudo yum -y install cpan
+yes "" | sudo cpan
+sudo cpan DBI DBD::mysql Module::Pluggable
+
+# Copy plugin config
+cp Makefile munin-mysql-master
+cp mysql.cnf munin-mysql-master
+cd munin-mysql-master && sudo make install
+
+
+
