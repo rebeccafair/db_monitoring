03065b6c024f08d57d9c96c82b9201e5c8fbded3
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2017-08-07 09:08:41 +0100

    Add Zabbix client install

--- zabbix/client/install_zabbix_client.sh
+++ zabbix/client/install_zabbix_client.sh
@@ -1,6 +1,11 @@
-sudo rpm -ivh http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm
-sudo yum install -y zabbix-agent
+#sudo rpm -ivh http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm
+#sudo yum install -y zabbix-agent
 
-sudo cp zabbix_agentd.conf /etc/zabbix/
-sudo systemctl enable zabbix-agent
-sudo systemctl restart zabbix-agent
+#sudo cp zabbix_agentd.conf /etc/zabbix/
+#sudo systemctl enable zabbix-agent
+#sudo systemctl restart zabbix-agent
+
+MYSQL_PASSWORD="Password!1"
+mysql -u root -p$MYSQL_PASSWORD -e "grant select, replication client on *.* to 'zabbix'@'localhost' identified by 'Password!1';"
+
+# Need to install plugin manually https://share.zabbix.com/databases/mysql/template-mysql-800-items
