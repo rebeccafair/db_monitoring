0885e34974decc351d1be8829a14f976f8b3b324
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2017-08-14 13:29:54 +0100

    Fix PMM Client install script

--- pmm/install_pmm_client.sh
+++ pmm/install_pmm_client.sh
@@ -2,10 +2,11 @@
 
 # Install client
 sudo yum install -y http://www.percona.com/downloads/percona-release/redhat/0.1-4/percona-release-0.1-4.noarch.rpm
+sudo yum install -y pmm-client
 
 # Connect to pmm server
-#sudo pmm-admin config --server pmm_server –client-name vm257
+sudo pmm-admin config --server vm265.nubes.stfc.ac.uk:8080 –client-name vm111
 
 # Add mysql monitoring
-#pmm-admin add mysql --create-user --password 'Password!1' 
+sudo pmm-admin add mysql --create-user --password 'Password!1' 
 
